7fb412898bbd64e975c2b89237968c43455597b8
yashenyu <markaarondayrit1130@gmail.com>
2025-07-06 02:32:32 +0800

    Initial
    Hell starts here.

---
+++ ImAged/Core/ObservableObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace ImAged.Core
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void onPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ ImAged/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImAged.Core;
+
+namespace ImAged.MVVM.ViewModel
+{ 
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+
+        public RelayCommand ViewViewCommand { get; set; }
+
+        public HomeViewModel HomeVm  { get; set; }
+
+        public ViewViewModel ViewVm { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                onPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVm = new HomeViewModel();
+            ViewVm = new ViewViewModel();
+            CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVm;
+            });
+
+            ViewViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ViewVm;
+            });
+        }
+    }
+}
