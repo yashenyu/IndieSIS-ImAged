0514e85aa1cfd59baf7cf85cc0f7817b2ce9e1d2
yashenyu <markaarondayrit1130@gmail.com>
2025-07-29 21:34:22 +0800

    added window controls in the topbar

--- ImAged/MVVM/ViewModel/MainViewModel.cs
+++ ImAged/MVVM/ViewModel/MainViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using ImAged.Core;
 
 namespace ImAged.MVVM.ViewModel
@@ -10,11 +11,11 @@ namespace ImAged.MVVM.ViewModel
     class MainViewModel : ObservableObject
     {
         public RelayCommand HomeViewCommand { get; set; }
-
         public RelayCommand ViewViewCommand { get; set; }
+        public RelayCommand CloseCommand { get; set; }
+        public RelayCommand MinimizeCommand { get; set; }
 
         public HomeViewModel HomeVm  { get; set; }
-
         public ViewViewModel ViewVm { get; set; }
 
         private object _currentView;
@@ -43,6 +44,18 @@ namespace ImAged.MVVM.ViewModel
             ViewViewCommand = new RelayCommand(o =>
             {
                 CurrentView = ViewVm;
+            });
+
+            CloseCommand = new RelayCommand(o =>
+            {
+                if (o is Window window)
+                    window.Close();
+            });
+
+            MinimizeCommand = new RelayCommand(o =>
+            {
+                if (o is Window window)
+                    window.WindowState = WindowState.Minimized;
             });
         }
     }
