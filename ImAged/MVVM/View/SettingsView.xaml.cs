aef9a41421e4b2f24184ba4a2967fabffe77b403
Andre Quizon <135997258+AnonymousDre@users.noreply.github.com>
2025-08-07 18:01:40 +0800

    Update SettingsView.xaml
    - added temporary UI for custom download path

--- ImAged/MVVM/View/SettingsView.xaml.cs
+++ ImAged/MVVM/View/SettingsView.xaml.cs
@@ -10,14 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 
 namespace ImAged.MVVM.View
 {
     /// <summary>
     /// Interaction logic for SettingsView.xaml
     /// </summary>
-    public partial class SettingsView : Window
+    public partial class SettingsView : UserControl
     {
         public SettingsView()
         {
--- ImAged/MVVM/ViewModel/SettingsViewModel.cs
+++ ImAged/MVVM/ViewModel/SettingsViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ImAged.MVVM.ViewModel
 {
-    internal class Class1
+    internal class SettingsViewModel
     {
     }
 }
