91e3c1b184617f46955189b71dbf6ce242c7f767
Andre Quizon <135997258+AnonymousDre@users.noreply.github.com>
2025-08-22 20:54:34 +0800

    Update FileView.xaml.cs and ViewModel.cs

--- ImAged/MVVM/View/FileView.xaml.cs
+++ ImAged/MVVM/View/FileView.xaml.cs
@@ -15,14 +15,12 @@ using System.Windows.Shapes;
 
 namespace ImAged.MVVM.View
 {
-    /// <summary>
-    /// Interaction logic for FileView.xaml
-    /// </summary>
     public partial class FileView : UserControl
     {
         public FileView()
         {
             InitializeComponent();
+            DataContext = new FileViewModel();
         }
     }
 }
--- ImAged/MVVM/ViewModel/FileViewModel.cs
+++ ImAged/MVVM/ViewModel/FileViewModel.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ImAged.MVVM.ViewModel
 {
-    internal class FileViewModel
+    public ObservableCollection<FileItem> Files { get; set; }
+
+    public FileViewModel()
     {
+        // Mock data for now
+        Files = new ObservableCollection<FileItem>
+        {
+            new FileItem { FileName = "image1.jpg", Modified = DateTime.Now, Status = "Active" },
+            new FileItem { FileName = "doc1.pdf", Modified = DateTime.Now.AddDays(-1), Status = "Archived" }
+        };
     }
 }
