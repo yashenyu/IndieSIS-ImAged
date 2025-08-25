3135a81e76fc21bb652b865cae45771537ccde30
Andre Quizon <135997258+AnonymousDre@users.noreply.github.com>
2025-08-25 22:50:28 +0800

    Update: SettingsView.xaml overhaul into an Developers Page

--- ImAged/MVVM/View/SettingsView.xaml.cs
+++ ImAged/MVVM/View/SettingsView.xaml.cs
@@ -18,35 +18,11 @@ using Forms = System.Windows.Forms;
 
 namespace ImAged.MVVM.View
 {
-    public partial class SettingsView : System.Windows.Controls.UserControl
+    public partial class SettingsView
     {
         public SettingsView()
         {
             InitializeComponent();
-
-            DownloadPathTextBox.Text = Properties.Settings.Default.DownloadPath ?? "No Path Selected";
-        }
-
-        private void SelectDownloadPath_Click(object sender, RoutedEventArgs e)
-        {
-            var dialog = new Forms.FolderBrowserDialog();
-            dialog.Description = "Select Download Path";
-
-            if (dialog.ShowDialog() == Forms.DialogResult.OK)
-            {
-                string selectedPath = dialog.SelectedPath;
-
-                // Display the selected path
-                DownloadPathTextBox.Text = selectedPath;
-
-                // Save to settings
-                Properties.Settings.Default.DownloadPath = selectedPath;
-                Properties.Settings.Default.Save();
-            }
-            else
-            {
-                System.Windows.MessageBox.Show("Selected path does not exist or was cancelled.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
-            }
         }
     }
 }
