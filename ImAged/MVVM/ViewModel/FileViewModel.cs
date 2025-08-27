e7f796892be524e6cae7f28f2b080d6e6ccf025b
yashenyu <markaarondayrit1130@gmail.com>
2025-08-27 15:59:21 +0800

    hot fix

--- ImAged/MVVM/ViewModel/FileViewModel.cs
+++ ImAged/MVVM/ViewModel/FileViewModel.cs
@@ -5,10 +5,6 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
 using System.Windows.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows;
 
 namespace ImAged.MVVM.ViewModel
 {
@@ -16,8 +12,6 @@ namespace ImAged.MVVM.ViewModel
     {
         private FileItem _selectedFile;
         private string _searchText;
-        private readonly List<string> _searchDirectories = new List<string>();
-        private readonly List<FileSystemWatcher> _fileWatchers = new List<FileSystemWatcher>();
 
         public FileItem SelectedFile
         {
@@ -55,17 +49,7 @@ namespace ImAged.MVVM.ViewModel
             FilesView = CollectionViewSource.GetDefaultView(Files);
             FilesView.Filter = FilterFiles;
 
-            InitializeSearchDirectories();
             LoadFiles();
-            SetupFileWatching();
-        }
-
-        private void InitializeSearchDirectories()
-        {
-            _searchDirectories.Clear();
-            _searchDirectories.Add(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures));
-            _searchDirectories.Add(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
-            _searchDirectories.Add(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads"));
         }
 
         private void LoadFiles()
