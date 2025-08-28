c5502772df0c848a7d95b521a5cbed730fcbd02d
Ric Pangan <ricpangan02@gmail.com>
2025-08-29 01:00:15 +0800

    Refactor settings view to use Developer model
    Introduced a Developer model and updated SettingsViewModel to provide an ObservableCollection of developers. Refactored SettingsView.xaml to dynamically display developer cards using data binding, and added new developer images. Also updated resource references in the project file and fixed assembly binding redirect in App.config.

---
+++ ImAged/MVVM/Model/Developer.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ImAged.MVVM.Model
+{
+    public class Developer
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public string School { get; set; } = string.Empty;
+        public string ImagePath { get; set; } = string.Empty;
+    }
+}
+
+
--- ImAged/MVVM/ViewModel/SettingsViewModel.cs
+++ ImAged/MVVM/ViewModel/SettingsViewModel.cs
@@ -1,12 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using ImAged.MVVM.Model;
 
 namespace ImAged.MVVM.ViewModel
 {
     internal class SettingsViewModel
     {
+        public ObservableCollection<Developer> Developers { get; } = new ObservableCollection<Developer>();
+
+        public SettingsViewModel()
+        {
+            // Seed with your four developers. Update ImagePath to correct images.
+            Developers.Add(new Developer
+            {
+                Name = "Dayrit, Mark Aaron B.",
+                Role = "Full Stack Developer",
+                School = "Holy Angel University",
+                ImagePath = "/Images/Mark.png"
+            });
+
+            Developers.Add(new Developer
+            {
+                Name = "Pangan, Ric Christian B.",
+                Role = "Front-End Developer",
+                School = "Holy Angel University",
+                ImagePath = "/Images/Ric.jpg"
+            });
+
+            Developers.Add(new Developer
+            {
+                Name = "Quizon, Andre Thomas M.",
+                Role = "Project Manager",
+                School = "Holy Angel University",
+                ImagePath = "/Images/dre.jpg"
+            });
+
+            Developers.Add(new Developer
+            {
+                Name = "Siron, Carlo S.",
+                Role = "UI/UX Designer Front-end Developer",
+                School = "Holy Angel University",
+                ImagePath = "/Images/"
+            });
+        }
     }
 }
